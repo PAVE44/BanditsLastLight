9e8e86fc2488a4ba0525829441ec6b80b06d362f
ppawlowski <ppawlowski@qualtrics.com>
2025-05-14 14:40:56 +0200

    bll

--- 42/media/lua/client/BLLVehicles.lua
+++ 42/media/lua/client/BLLVehicles.lua
@@ -1,8 +1,17 @@
 BLLVehicles = BLLVehicles or {}
 
-BLLVehicles.tab = {}
+-- const
+BLLVehicles.speedMin = 2
+BLLVehicles.speedMax = 10
+BLLVehicles.reloadTime = 100
+BLLVehicles.ammoContainerPartName = "" -- fixme
+BLLVehicles.ammoItemTypeName = "" -- fixme
+BLLVehicles.ammoBoxItemTypeName = "" -- fixme
+BLLVehicles.ammoBoxSize = 100 -- fixme
 
+BLLVehicles.tab = {}
 BLLVehicles.tick = 0
+BLLVehicles.reloadTick = 0
 
 BLLVehicles.Repair = function(vehicle)
     -- we cant use vehicle:replair() because it will add armor to ki5 vehicles
@@ -84,6 +93,12 @@ BLLVehicles.Spawn = function(x, y, dir, btype)
     end
 end
 
+BLLVehicles.Embark = function(vehicle, bandit)
+end
+
+BLLVehicles.Disembark = function(vehicle)
+end
+
 local function sign(x)
     if x > 0 then
         return 1
@@ -94,9 +109,19 @@ local function sign(x)
     end
 end
 
-function normalize(angle)
+local function normalize(angle)
     angle = (angle + 180) % 360 - 180
     return angle
+end
+
+local function predicateAmmo(item)
+    if item:getType() == BLLVehicles.ammoItemTypeName then return true end
+    return true
+end
+
+local function predicateAmmoBox(item)
+    if item:getType() == BLLVehicles.ammoBoxItemTypeName then return true end -- fixme
+    return true
 end
 
 local function manageEngineSmoke(vehicle)
@@ -124,33 +149,15 @@ local function manageEngineSmoke(vehicle)
     end
 end
 
-function reverseVehicleRotation(rotation)
-    rotation = rotation % 360
-
-    -- We'll produce both possibilities
-    local results = {}
-
-    -- Case 1: abs(x) >= 45
-    local y1 = (rotation - 270) % 360
-    table.insert(results, {angleX = 45, angleY = y1, angleZ = 0})
-
-    -- Case 2: abs(x) < 45
-    local y2 = (90 - rotation) % 360
-    table.insert(results, {angleX = 0, angleY = y2, angleZ = 0})
-
-    return results
-end
-
 local function manageDriver(square, vehicle, driver)
 
     local vehicleList = BLLVehicles.tab
-    local cell = square:getCell()
     local vid = vehicle:getId()
     local vx = vehicle:getX()
     local vy = vehicle:getY()
 
     local slot = 1
-    for id, vehicle in pairs(vehicleList) do
+    for id, _ in pairs(vehicleList) do
         if id ~= vid and vehicle:getX() < vx then
             slot = slot + 1
         end
@@ -167,73 +174,38 @@ local function manageDriver(square, vehicle, driver)
     local dist = BanditUtils.DistTo(vx, vy, target.x, target.y)
     local tvr = BanditUtils.CalcAngle (vx, vy, target.x, target.y)
     local delta = normalize(normalize(tvr) - normalize(vr))
-    
+
     if dist < 2 then
         vehicle:setRegulator(false)
         vehicle:setRegulatorSpeed(0)
     elseif math.abs(delta) < 2 then
-        local speed = dist < 10 and 5 or 12
+        local speed = dist / 10
+        if speed < BLLVehicles.speedMin then speed = BLLVehicles.speedMin end
+        if speed > BLLVehicles.speedMax then speed = BLLVehicles.speedMax end
+
         vehicle:setRegulator(true)
         vehicle:setRegulatorSpeed(speed)
-        print ("vid: " .. vid .. "slot: " .. slot)
+        -- print ("vid: " .. vid .. "slot: " .. slot)
     else
-        local step = (delta > 0) and 1 or -1
-        local nva = (90 - normalize(vr + step)) % 360
         vehicle:setRegulator(false)
         vehicle:setRegulatorSpeed(0)
-        vehicle:setAngles(0, nva, 0)
-    end
-
-    if false then
-        local vecs = dirMap[dir]
-
-        -- stop if needed
-        for _, vec in pairs(vecs) do
-            local asquare = cell:getGridSquare(vx + vec.x, vy + vec.y, 0)
-            if asquare then
-                local shouldStop = false
-
-                if not asquare:isFree(false) or asquare:isVehicleIntersecting() then
-                    shouldStop = true
-                elseif (asquare:getPlayer() and not asquare:getPlayer():isNPC()) then
-                    local emitter = vehicle:getEmitter()
-                    if not emitter:isPlaying("VehicleHornStandard") then
-                        emitter:playSound("VehicleHornStandard")
-                    end
-                    shouldStop = true
-                end
-
-                if shouldStop then
-                    vehicle:setRegulatorSpeed(0)
-                    vehicle:setRegulator(false)
-                    return
-                end
-            end
-        end
-
-        -- drive forward
-        local emitter = vehicle:getEmitter()
-        if emitter:isPlaying("VehicleHornStandard") then
-            emitter:stopSoundByName("VehicleHornStandard")
-        end
         if vehicle:isStopped() then
-            vehicle:setRegulator(true)
-            vehicle:setRegulatorSpeed(BLLVars.Get("convoySpeed"))
+            local step = (delta > 0) and 1 or -1
+            local nva = (90 - normalize(vr + step)) % 360
+            vehicle:setAngles(0, nva, 0)
         end
     end
 
-    -- engine smoke
-    manageEngineSmoke(vehicle)
 end
 
 local function setDriver(square, vehicle)
-    
-    local npcAesthetics = SurvivorFactory.CreateSurvivor(SurvivorType.Neutral, false)
-    npcAesthetics:setForename("Driver")
-    npcAesthetics:setSurname("Driver")
-    npcAesthetics:dressInNamedOutfit("Police")
 
-    local driver = IsoPlayer.new(cell, npcAesthetics, square:getX(), square:getY(), square:getZ())
+    local survDesc = SurvivorFactory.CreateSurvivor(SurvivorType.Neutral, false)
+    survDesc:setForename("Driver")
+    survDesc:setSurname("Driver")
+    survDesc:dressInNamedOutfit("Police")
+
+    local driver = IsoPlayer.new(square:getCell(), survDesc, square:getX(), square:getY(), square:getZ())
 
     driver:setSceneCulled(false)
     driver:setNPC(true)
@@ -244,7 +216,7 @@ local function setDriver(square, vehicle)
     local vx = driver:getForwardDirection():getX()
     local vy = driver:getForwardDirection():getY()
     local forwardVector = Vector3f.new(vx, vy, 0)
-    
+
     if vehicle:getChunk() then
         vehicle:setPassenger(0, driver, forwardVector)
         driver:setVehicle(vehicle)
@@ -260,101 +232,129 @@ local function setDriver(square, vehicle)
 end
 
 local function manageGunner(square, vehicle, gunner)
-    local bestDist = 40
-    local enemyCharacter
     local vx = vehicle:getX()
     local vy = vehicle:getY()
     local vz = vehicle:getZ()
     local vid = vehicle:getId()
     local cell = square:getCell()
 
+    -- manage reload if necessary
+    local ammoItems = ArrayList.new()
+    local ammoContainer = vehicle:getPartById(BLLVehicles.ammoContainerPartName):getItemContainer()
+    ammoContainer:getAllEvalRecurse(predicateAmmo, ammoItems)
+    local ammoLeft = ammoItems:size()
+    if ammoLeft == 0 then
+        local ammoBoxItems = ArrayList.new()
+        ammoContainer:getAllEvalRecurse(predicateAmmoBox, ammoBoxItems)
+        local ammoBoxLeft = ammoBoxItems:size()
+        if ammoBoxLeft > 0 then
+            BLLVehicles.reloadTick = BLLVehicles.reloadTick + 1
+            if BLLVehicles.reloadTick == BLLVehicles.reloadTime then
+                ammoContainer:RemoveOneOf(BLLVehicles.ammoBoxItemTypeName, true)
+                local cnt = BLLVehicles.ammoBoxSize
+                for i=1, cnt do
+                    local item = BanditCompatibility.InstanceItem(BLLVehicles.ammoItemTypeName)
+                    ammoContainer:AddItem(item)
+                end
+                BLLVehicles.reloadTick = 0
+            end
+        end
+    end
+
+    if ammoLeft == 0 then return end
+
+    -- detect enemies
+    local bestDist = 40
+    local enemyCharacter
     local cache, potentialEnemyList = BanditZombie.Cache, BanditZombie.CacheLight
     for id, potentialEnemy in pairs(potentialEnemyList) do
         if math.abs(potentialEnemy.x - vx) + math.abs(potentialEnemy.y - vy) < 57 then
             if not potentialEnemy.brain or potentialEnemy.brain.hostile then
-                local potentialEnemy = cache[id]
-                local px, py, pz = potentialEnemy:getX(), potentialEnemy:getY(), potentialEnemy:getZ()
-                local dist = math.sqrt(((vx - px) * (vx - px)) + ((vy - py) * (vy - py)))
-                if dist < bestDist then
-                    bestDist, enemyCharacter = dist, potentialEnemy
+                local zombie = cache[id]
+                if gunner:CanSee(zombie) then
+                    local px, py, pz = zombie:getX(), zombie:getY(), zombie:getZ()
+                    local dist = math.sqrt(((vx - px) * (vx - px)) + ((vy - py) * (vy - py)))
+                    if dist < bestDist then
+                        bestDist, enemyCharacter = dist, zombie
+                    end
                 end
             end
         end
     end
 
-    if enemyCharacter then
-        local dx = enemyCharacter:getX() - vx
-        local dy = enemyCharacter:getY() - vy
-        local aimAngle = math.deg(math.atan2(dy, dx))
+    if not enemyCharacter then return end
 
-        -- local aimAngle = player:getDirectionAngle()
-        -- aimAngle = (aimAngle + 720) % 360
+    -- manage turret rotation and firing
+    local dx = enemyCharacter:getX() - vx
+    local dy = enemyCharacter:getY() - vy
+    local aimAngle = math.deg(math.atan2(dy, dx))
 
-        local md = vehicle:getModData()
-        local currentRotation = md.BLL.turretDir
+    -- local aimAngle = player:getDirectionAngle()
+    -- aimAngle = (aimAngle + 720) % 360
 
-        local angleX = vehicle:getAngleX()
-        local angleY = vehicle:getAngleY()
-        local angleZ = vehicle:getAngleZ()
-        local vehicleRotation = calculateVehicleRotation(angleX, angleY, angleZ)
+    local md = vehicle:getModData()
+    local currentRotation = md.BLL.turretDir
 
-        local targetRotation = (vehicleRotation - aimAngle + 720) % 360
+    local angleX = vehicle:getAngleX()
+    local angleY = vehicle:getAngleY()
+    local angleZ = vehicle:getAngleZ()
+    local vehicleRotation = calculateVehicleRotation(angleX, angleY, angleZ)
 
-        local deltaTime = 1
-        local rotationSpeed = 6
-        local rotationStep = rotationSpeed * deltaTime
-        local angleDifference = ((targetRotation - currentRotation + 540) % 360) - 180
-    
-        local newRotation
-        local firing = false
-        if math.abs(angleDifference) <= rotationStep then
-            newRotation = targetRotation
-            firing = true
-        else
-            newRotation = (currentRotation + sign(angleDifference) * rotationStep + 360) % 360
-        end
-        md.BLL.turretDir = newRotation
+    local targetRotation = (vehicleRotation - aimAngle + 720) % 360
 
-        local turretWorldAngle = newRotation
-        local currentAnimID = math.floor(turretWorldAngle / 2) * 2 + 2
-        if currentAnimID > 360 then
-            currentAnimID = 2
-        elseif currentAnimID < 2 then
-            currentAnimID = 360
-        end
+    local deltaTime = 1
+    local rotationSpeed = 6
+    local rotationStep = rotationSpeed * deltaTime
+    local angleDifference = ((targetRotation - currentRotation + 540) % 360) - 180
 
-        if currentAnimID ~= md.BLL.lastAnimID then
-            local animIDFormatted = string.format("%03d", currentAnimID)			
-            vehicle:playPartAnim(vehicle:getPartById("Turrent"), animIDFormatted)
+    local newRotation
+    local firing = false
+    if math.abs(angleDifference) <= rotationStep then
+        newRotation = targetRotation
+        firing = true
+    else
+        newRotation = (currentRotation + sign(angleDifference) * rotationStep + 360) % 360
+    end
+    md.BLL.turretDir = newRotation
 
-            md.BLL.lastAnimID = currentAnimID
-        end
+    local turretWorldAngle = newRotation
+    local currentAnimID = math.floor(turretWorldAngle / 2) * 2 + 2
+    if currentAnimID > 360 then
+        currentAnimID = 2
+    elseif currentAnimID < 2 then
+        currentAnimID = 360
+    end
 
-        if firing and BLLVehicles.tick % 12 > 0 then
-            BanditProjectile.Add(vid, vx, vy, vz, aimAngle, 1)
-            local emitter = getWorld():getFreeEmitter(vx, vy, vz)
-            emitter:playSound("BLL_M2_Fire")
+    if currentAnimID ~= md.BLL.lastAnimID then
+        local animIDFormatted = string.format("%03d", currentAnimID)
+        vehicle:playPartAnim(vehicle:getPartById("Turrent"), animIDFormatted)
 
-            local lightSource = IsoLightSource.new(vx, vy, vz, 1.0, 0.9, 0.8, 18, 2)
-            cell:addLamppost(lightSource)
-            
-            addSound(getSpecificPlayer(0), vx, vy, vz, 40, 100)
-        
-            weaponItem = BanditCompatibility.InstanceItem("Base.HuntingRifle")
-            BanditUtils.ManageLineOfFire(gunner, enemyCharacter, weaponItem)
-            gunner:playSound(weaponItem:getShellFallSound())
-        end
+        md.BLL.lastAnimID = currentAnimID
+    end
+
+    if firing and BLLVehicles.tick % 12 > 0 then
+        BanditProjectile.Add(vid, vx, vy, vz, aimAngle, 1)
+        local emitter = getWorld():getFreeEmitter(vx, vy, vz)
+        emitter:playSound("BLL_M2_Fire")
+
+        local lightSource = IsoLightSource.new(vx, vy, vz, 1.0, 0.9, 0.8, 18, 2)
+        cell:addLamppost(lightSource)
+
+        addSound(getSpecificPlayer(0), vx, vy, vz, 40, 100)
+
+        weaponItem = BanditCompatibility.InstanceItem("Base.HuntingRifle")
+        BanditUtils.ManageLineOfFire(gunner, enemyCharacter, weaponItem)
+        gunner:playSound(weaponItem:getShellFallSound())
     end
 end
 
 local function setGunner(square, vehicle)
-    
-    local npcAesthetics = SurvivorFactory.CreateSurvivor(SurvivorType.Neutral, false)
-    npcAesthetics:setForename("Gunner")
-    npcAesthetics:setSurname("Gunner")
-    npcAesthetics:dressInNamedOutfit("Police")
+    local survDesc = SurvivorFactory.CreateSurvivor(SurvivorType.Neutral, false)
+    survDesc:setForename("Gunner")
+    survDesc:setSurname("Gunner")
+    survDesc:dressInNamedOutfit("Police")
 
-    local gunner = IsoPlayer.new(cell, npcAesthetics, square:getX(), square:getY(), square:getZ())
+    local gunner = IsoPlayer.new(square:getCell(), survDesc, square:getX(), square:getY(), square:getZ())
 
     gunner:setSceneCulled(false)
     gunner:setNPC(true)
@@ -365,7 +365,7 @@ local function setGunner(square, vehicle)
     local vx = gunner:getForwardDirection():getX()
     local vy = gunner:getForwardDirection():getY()
     local forwardVector = Vector3f.new(vx, vy, 0)
-    
+
     if vehicle:getChunk() then
         vehicle:setPassenger(1, gunner, forwardVector)
         gunner:setVehicle(vehicle)
@@ -400,25 +400,11 @@ local Manage = function(ticks)
         local square = vehicle:getSquare()
         if not square then return end
 
+        -- engine smoke
+        manageEngineSmoke(vehicle)
+
         local driver = vehicle:getDriver()
         if driver and driver:isNPC() then
-
-            local dist = BanditUtils.DistToManhattan(player:getX(), player:getY(), vehicle:getX(), vehicle:getY())
-            if dist > 51 then
-                local seat = vehicle:getSeat(driver)
-                vehicle:clearPassenger(seat)
-                driver:setVehicle(nil)
-                driver:setCollidable(true)
-                --driver:setHealth(0)
-                driver:Kill(nil)
-                driver:removeSaveFile()
-                driver:removeFromSquare()
-                driver:removeFromWorld()
-                vehicle:permanentlyRemove()
-                BLLVehicles.tab[id] = nil
-                break
-            end
-
             manageDriver(square, vehicle, driver)
         else
             setDriver(square, vehicle)
